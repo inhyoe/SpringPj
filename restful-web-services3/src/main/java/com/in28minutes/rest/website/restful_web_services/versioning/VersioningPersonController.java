320a78d47f0136ab5a50fb4edbbaf2d764811e1e
inhyoe <melon7256@naver.com>
2024-06-07 14:13:35 +0900

    버전에 따른 관리 쿼리스트링을 이용한 방법, 헤더를 이용하는 방법, Accept-Header을 이용하는 방법

--- restful-web-services3/src/main/java/com/in28minutes/rest/website/restful_web_services/versioning/VersioningPersonController.java
+++ restful-web-services3/src/main/java/com/in28minutes/rest/website/restful_web_services/versioning/VersioningPersonController.java
@@ -3,6 +3,11 @@ package com.in28minutes.rest.website.restful_web_services.versioning;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * content negotiation
+ * 콘텐츠 협상
+ * 다양한 방법으로 버전을 관리하려는 경우
+ */
 @RestController
 public class VersioningPersonController {
 	@GetMapping("/v1/person")
@@ -14,4 +19,38 @@ public class VersioningPersonController {
 	public PersonV2 getPersonV2() {
 		return new PersonV2(new Name("Ingype", "Man"));
 	}
+
+	/**
+	 * 쿼리스트링을 이용한 방법
+	 * @return
+	 */
+	@GetMapping(path = "/person", params = "verson=1")
+	public PersonV1 getPersonV1s() {
+		return new PersonV1("ingyoe");
+	}
+
+	@GetMapping(path = "/person", params = "verson=2")
+	public PersonV2 getPersonV2s() {
+		return new PersonV2(new Name("Ingype", "Man"));
+	}
+
+	/**
+	 * 헤더를 이용한 방법
+	 * @return
+	 */
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+	public PersonV1 getPersonV1RequestHeader() {
+		return new PersonV1("ingyoe");
+	}
+
+	@GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+	public PersonV2 getPersonV2RequestHeader() {
+		return new PersonV2(new Name("Ingype", "Man"));
+	}
+
+	@GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 getPersonV2AcceptHeader() {
+		return new PersonV2(new Name("Ingype", "Man"));
+	}
+
 }
